d30f298d63f8aee442dd6d47169d7a7f3000d4d4
AngMingZe <angmingze21@gmail.com>
2022-04-29 14:32:08 +0800

    Practical 2 part 1 (still have follow button)

--- app/src/main/java/com/example/s10203953/MainActivity.java
+++ app/src/main/java/com/example/s10203953/MainActivity.java
@@ -4,9 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    public class User{
+        //Attr
+        String name;
+        String description;
+        int id;
+        boolean followed;
+        //Constructor
+        public User(){};
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +26,13 @@ public class MainActivity extends AppCompatActivity {
 
     public void anyName(View v){
         //Event handler
-        //test push stuff
+        Button b1 = findViewById(R.id.follow);
+        String text = b1.getText().toString();//Text of follow button
+        if (text == "Follow"){
+            b1.setText("Unfollow");
+        }
+        else if (text == "Unfollow"){
+            b1.setText("Follow");
+        }
     }
 }
